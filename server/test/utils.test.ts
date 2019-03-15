eae8ee7bb5b6c49525b99933fae8e3c8737af386
Georgi Krustev <ggkrustev@yahoo.com>
2019-03-15 14:40:39 +0200

    test(server): add mock test

---
+++ server/test/utils.test.ts
@@ -0,0 +1,8 @@
+import { expect } from 'chai'
+import 'mocha'
+
+describe('Test function', () => {
+    it('should return hello world', () => {
+        expect('hello').to.equal('hello')
+    })
+})
