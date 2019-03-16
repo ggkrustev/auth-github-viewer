8b21bb552c2ab2beb8dd9f93c5ce22667f6d2b0e
Georgi Krustev <ggkrustev@yahoo.com>
2019-03-16 22:04:38 +0200

    feat(server): bootstrap inversify container

--- server/src/di.ts
+++ server/src/di.ts
@@ -1,3 +1,7 @@
 export default {
-    FooService: Symbol.for('FooService'),
+    GITHUB_COMMIT_PATCH: Symbol.for('GithubCommitPatch'),
+    GRAPHQL_CLIENT: Symbol.for('GraphQLClient'),
+    GITHUB_CONFIG: Symbol.for('GithubConfig'),
+    GITHUB_SERVICE: Symbol.for('GithubService'),
+    SERVER_CONFIG: Symbol.for('ServerConfig'),
 }
--- server/src/injections.ts
+++ server/src/injections.ts
@@ -1,11 +1,43 @@
 import { Container } from 'inversify'
+import { GraphQLClient } from 'graphql-request'
+import config from 'config'
+import DI from './di'
 
-// import DI from './di';
-// import { FooService } from './services/foo.service';
+import { GithubConfig, ServerConfig } from './config/interfaces'
+import { CommitPatchService } from './services/commit-patch.service'
+import { GithubService } from './services/github.service'
 
 const container = new Container()
 
-// container.bind<FooService>(DI.FooService).to(FooService);
+// Configuration
+container.bind<GithubConfig>(DI.GITHUB_CONFIG).toConstantValue({
+    token: config.get('github.token'),
+    apiUrl: config.get('github.apiUrl'),
+    organizationUrl: config.get('github.organizationUrl')
+});
+
+container.bind<ServerConfig>(DI.SERVER_CONFIG).toConstantValue({
+    port: config.get('server.port')
+});
+
+// Services
+container.bind<GraphQLClient>(DI.GRAPHQL_CLIENT)
+         .toDynamicValue(
+           (context) => {
+              const githubConfig = context.container.get<GithubConfig>(DI.GITHUB_CONFIG);
+              const { apiUrl, token } = githubConfig;
+
+              return new GraphQLClient(apiUrl, {
+                  headers: {
+                      Authorization: `Bearer ${token}`,
+                  },
+              });
+           }
+         );
+
+container.bind<CommitPatchService>(DI.GITHUB_COMMIT_PATCH).to(CommitPatchService);
+container.bind<GithubService>(DI.GITHUB_SERVICE).to(GithubService);
+
 
 export default container
 
